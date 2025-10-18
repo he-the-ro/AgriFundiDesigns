0c4b7933f6fc06607617a5967e372f3c2f6bf23f
Heri <new@news-MacBook-Pro.local>
2025-10-19 01:28:08 +0300

    updated vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/AgriFundiDesigns/',
+    base: '/AgriFundi/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
